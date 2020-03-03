406cb2da8fa9998073837608e41dc8b6eb5e1cca
Sergio Martín Ortega <sergio.mortega@babel.es>
2020-03-03 19:15:09 +0100

    icono seleccionable

--- webpack-demo/src/index.ts
+++ webpack-demo/src/index.ts
@@ -1,13 +1,23 @@
 
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
-const openDelete = document.querySelector('.material-icons btn-delete');
-const deleteTask =  document.querySelector('.deleted-container')
+
 
 function countTask() {
   const tasks = document.getElementById('tasks');
   const allList = document.getElementsByTagName('li');
   tasks.textContent = 'Quedan '+ allList.length + ' tareas';
+}
+
+function openContainer(x) {
+  
+  x.addEventListener('click', () => {
+    //console.log('Hace algo 1'+x.setAttribute('id'));
+    const deleteTask =  document.querySelector('#deleted-container')
+    deleteTask.classList.add('open');
+    const nameTask = document.querySelector('#nameTask');
+    nameTask.textContent = 'prueba ';
+  });
 }
 
 input.onkeyup = (e) => {
@@ -20,11 +30,13 @@ input.onkeyup = (e) => {
     const icon = document.createElement('i');
     const check = document.createElement('input');
 
+    listItem.setAttribute('id', inputElement.value);
     console.log(inputElement.value);
     span.textContent = inputElement.value;
     check.setAttribute('type', 'checkbox');
     icon.textContent = 'delete_outline';
     icon.setAttribute('class', 'material-icons btn-delete');
+    icon.setAttribute('id', inputElement.value);
 
     list.appendChild(listItem);
     listItem.appendChild(div);
@@ -33,12 +45,6 @@ input.onkeyup = (e) => {
     div.appendChild(span);
     input.value = '';
     countTask();
+    openContainer(icon);
   }
 }
-
-openDelete.addEventListener('click', function () {
-  deleteTask.classList.add('open');
-});
-
-
-
